7578ed4635ff249ada1207164dc33d7aaf1b06a6
Sara <sluisdejesus@gmail.com>
2021-07-01 15:22:42 +0100

    mvp done all working

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,8 +1,22 @@
-from flask import Flask
-from flask.templating import render_template
+from flask import render_template, request, redirect
+from werkzeug.utils import redirect
 from app import app
-from models.events_list import Events
+from models.events_list import *
+from models.event import Event
 
 @app.route("/events")
 def index():
     return render_template ('index.html', title = "Home", events=Events)
+
+@app.route('/events',methods=['POST'])
+def add_event():
+    date = request.form["date"]
+    name_of_event= request.form["name_of_event"]
+    number_of_guests = request.form["number_of_guests"]
+    room_location = request.form["room_location"]
+    description = request.form["description"]
+    new_event = Event(date,name_of_event,number_of_guests,room_location,description)
+    add_new_event(new_event)
+
+    # return render_template("index.html", title="Home", events=Events)
+    return redirect("/events")
--- models/events_list.py
+++ models/events_list.py
@@ -1,7 +1,10 @@
-
+from models.event import *
 
 event_1 = Event('2021/7/1', "Download", 5000, "outside", "Metalhead Festival")
 event_2 = Event('2021/12/25', "Easter", 12, "Hell", "Eat chocolate until you pop")
 event_3 = Event('2021/10/10', "My birthday", 25, "Big Ben","Bring your own drinks")
 
 Events = [event_1,event_2, event_3]
+
+def add_new_event(event):
+    Events.append(event)
